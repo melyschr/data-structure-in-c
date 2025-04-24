123b39f39d19a9dd51b0abe137716585c6883d28
melyschr <melschramm8@gmail.com>
2025-04-23 22:49:59 -0300

    Add contingsort.c

---
+++ CoutingSort.c
@@ -0,0 +1,4 @@
+// Couting sort depende do tamanho do vetor em relação ao tempo de processamento, o tempo de processamento depende se e o vetor for maior
+// Algortimos rapidos usam esse tipo de estrategia então é bom
+
+#include <stdio.h>

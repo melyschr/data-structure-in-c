966a3ccf6edbdf6fa3b008d23ede768e53508546
melyschr <melschramm8@gmail.com>
2025-04-24 21:48:02 -0300

    Add coutingSort.c for tests

--- CoutingSort.c
+++ CoutingSort.c
@@ -1,4 +1,35 @@
-// Couting sort depende do tamanho do vetor em relação ao tempo de processamento, o tempo de processamento depende se e o vetor for maior
-// Algortimos rapidos usam esse tipo de estrategia então é bom
+// THIS CODE IS ONLY FOR ANY TESTS AND SEARCH/STUDY, THANKS!
 
-#include <stdio.h>
+#define MAX 50
+
+void countingSort (int *V, int N){
+    int i, j, k;
+    int C[MAX];
+    
+    for (i = 0; i < MAX; i++){
+        C[i] = 0;
+    }
+    for(i = 0; i < N; i++){
+        C[V[i]]++;
+    }
+    for (i = 0, j = 0; j < MAX; j++){
+        for (k = C[j]; k > 0; k--){
+            V[i++] = j;
+        }
+    }
+}
+
+void printArray(int C[], int size){
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", C[i]);
+}
+int main(){
+    int C[MAX];
+    int N = sizeof(C) / sizeof(C[0]);
+    printArray(C,N);
+    countingSort(C,N);
+    printArray(C,N);
+
+    return 0;
+}
